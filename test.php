d6f75c0721565af9f43f68b8eb1cf508976811fd
3umaHoro <102548106+3umaHoro@users.noreply.github.com>
2022-03-28 23:19:38 +0500

    Update test.php

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php
     echo "Project\n";
     echo "Semenova Ann\n";
-    echo "Рузин Данил Евгеньевич\n";<br/>
+    echo "Рузин Данил Евгеньевич\n<br/>";
     echo "Титарев Андрей Андреевич\n";
   ?>
 
