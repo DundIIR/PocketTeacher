86c30863551d74812d8485e50393f49b2c216161
Ann <annasem@mail.ru>
2022-05-23 19:46:58 +0300

    bg

--- mail.php
+++
@@ -1,8 +0,0 @@
-<?php
-$to = "abc@gmail.com";
-$subject = "Robot - Робот";
-$message = "Hello World!<br /><i>Это письмо отправлено <b>роботом</b>
-  и отвечать на него не нужно!</i>";
-$headers = "From: PocketTeacher";
-mail ($to, $subject, $message, $headers);
-?>
