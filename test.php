b85b172724bf814db2a4ba374b216eb652e0bfe5
danil <ruzin_de@edu.surgu.ru>
2022-05-25 10:25:55 +0500

    пользовательский профиль

--- test.php
+++ test.php
@@ -3,7 +3,9 @@
     if(!$_SESSION['user']){
     header('Location: index.php');
 }
+    require_once 'reg/connect.php';
 ?>
+
 
 <!doctype html>
 <html lang="ru">
@@ -16,21 +18,151 @@
     <link rel="stylesheet" href="css/style_profile.css">
 </head>
 <body>
-<header>
-    <div class="container">
-        <div class="header_inner">
-            <div class="header_logo"><img src="images/log2.jpg" id="logoTwo" alt="Picture" height="50px">Pocket Teacher</div>
+<div class="wrap">
+    <div class="header">
+        <div class="container">
+            <div class="header_inner">
+                <div class="header_logo"><img src="images/log2.jpg" id="logoTwo" alt="Picture" height="50px">Pocket Teacher</div>
 
-            <naw class="nav">
-                <a class="nav_link" href="#">Добавить</a>
-                <a class="nav_link" href="#">Изменить</a>
-                <a class="nav_link" href="#">Удалить</a>
+                <naw class="nav">
+                    <a class="nav_link" href="#">Добавить</a>
+                    <a class="nav_link" href="#">Изменить</a>
+                    <a class="nav_link" href="#">Удалить</a>
+                    <a class="nav_link" href="exit.php">Выход</a>
 
-            </naw>
+                </naw>
+            </div>
         </div>
     </div>
-</header>
-<a class="exit" href="exit.php">Выход</a>
+    <div class="container" id="container">
+        <div class="style">
+            <hr>
+            <div class="state">
+                <h4> Для чего и зачем нужен английский язык?<br /></h4>
+                <div> Некоторые люди говорят, что очень необходимо изучать иностранные языки, но другие говорят, что учить иностранные языки не важны.
+                    Что же насчет нас, то мы согласены с людьми, которые говорят о преимуществах изучения иностранных языков.
+
+                    Во-первых, знание иностранного языка может помочь нам понимать людей со всего мира. Мы можем поддерживать связь с носителями языка.
+                    Мы можем изучать историю их наций. Во-вторых, мы можем читать иностранную литературу в оригинале.
+                    Это более интересно, ведь мы можем понять мысли автора, не переводчика. В-третьих, английский язык-язык игр, спорта, красоты и бизнеса.
+                    Мы можем принять участие в международных спортивных соревнованиях, модельных показах или бизнес переговорах и мы можем быть уверены, что будем поняты верно, без каких-либо ошибок.
+                    <br /> </div>
+            </div>
+
+
+            <div> В заключении мы бы хотели подчеркнуть, что изучение иностранных языков играет большую роль в современной жизни, потому что весь мир связан разными путями и мы должны поддерживать связь с людьми со всех уголков света.
+                <br /> </div>
+           <img src="images/tm.jpg" alt="Picture" height="400px" class="image_center"></a>
+        </div>
+        <div class="table_had">
+            <form action="">
+                <input type="text" class="form-control" name="room"
+                       id="room" placeholder="Введите номер"><br>
+                <input type="text" class="form-control" name="word"
+                       id="word" placeholder="Введите слово">
+                <input type="text" class="form-control" name="translate"
+                       id="translate" placeholder="Введите перевод"><br>
+                <button class="nav_link" type="submit">Добавить</button>
+            </form>
+        </div>
+        <div class="table overflow">
+            <table>
+                <tr>
+                    <th>№</th>
+                    <th>Слово</th>
+                    <th>Перевод</th>
+                </tr>
+                <tr>
+                    <?php
+                        $word_and_translate = mysqli_query($connect, "SELECT * FROM `word_and_translate`");
+                        $word_and_translate = mysqli_fetch_all($word_and_translate);
+                        foreach ($word_and_translate as $item) {
+                            ?>
+                            <tr>
+                                <td><?=$item[0] ?></td>
+                                <td><?=$item[2] ?></td>
+                                <td><?=$item[3] ?></td>
+                            </tr>
+                <tr>
+                    <td><?=$item[0] ?></td>
+                    <td><?=$item[2] ?></td>
+                    <td><?=$item[3] ?></td>
+                </tr>
+                <tr>
+                    <td><?=$item[0] ?></td>
+                    <td><?=$item[2] ?></td>
+                    <td><?=$item[3] ?></td>
+                </tr>
+                <tr>
+                    <td><?=$item[0] ?></td>
+                    <td><?=$item[2] ?></td>
+                    <td><?=$item[3] ?></td>
+                </tr>
+                <tr>
+                    <td><?=$item[0] ?></td>
+                    <td><?=$item[2] ?></td>
+                    <td><?=$item[3] ?></td>
+                </tr>                           <tr>
+                    <td><?=$item[0] ?></td>
+                    <td><?=$item[2] ?></td>
+                    <td><?=$item[3] ?></td>
+                </tr>                           <tr>
+                    <td><?=$item[0] ?></td>
+                    <td><?=$item[2] ?></td>
+                    <td><?=$item[3] ?></td>
+                </tr>
+                <tr>
+                    <td><?=$item[0] ?></td>
+                    <td><?=$item[2] ?></td>
+                    <td><?=$item[3] ?></td>
+                </tr>
+                <tr>
+                    <td><?=$item[0] ?></td>
+                    <td><?=$item[2] ?></td>
+                    <td><?=$item[3] ?></td>
+                </tr>                           <tr>
+                    <td><?=$item[0] ?></td>
+                    <td><?=$item[2] ?></td>
+                    <td><?=$item[3] ?></td>
+                </tr>                           <tr>
+                    <td><?=$item[0] ?></td>
+                    <td><?=$item[2] ?></td>
+                    <td><?=$item[3] ?></td>
+                </tr>
+                <tr>
+                    <td><?=$item[0] ?></td>
+                    <td><?=$item[2] ?></td>
+                    <td><?=$item[3] ?></td>
+                </tr>
+                <tr>
+                    <td><?=$item[0] ?></td>
+                    <td><?=$item[2] ?></td>
+                    <td><?=$item[3] ?></td>
+                </tr>                           <tr>
+                    <td><?=$item[0] ?></td>
+                    <td><?=$item[2] ?></td>
+                    <td><?=$item[3] ?></td>
+                </tr>                           <tr>
+                    <td><?=$item[0] ?></td>
+                    <td><?=$item[2] ?></td>
+                    <td><?=$item[3] ?></td>
+                </tr>
+                <tr>
+                    <td><?=$item[0] ?></td>
+                    <td><?=$item[2] ?></td>
+                    <td><?=$item[3] ?></td>
+                </tr>
+
+
+
+                            <?php
+                        }
+                    ?>
+                </tr>
+            </table>
+        </div>
+    </div>
+</div>
 
 
 </body>
